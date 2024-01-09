e3bd91c0ebbb213f46650304dc193aa9e78c8437
amir daaee <amir.daaee@gmail.com>
2024-01-09 13:22:23 +0330

    v2ray update bug fix

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
-pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export JSON_FILE=data/goog.json
 export TEXT_IPV4_FILE=data/ipv4.txt
 export TEXT_IPV6_FILE=data/ipv6.txt
 mkdir -p ./data
 wget -O $JSON_FILE https://www.gstatic.com/ipranges/goog.json
 python3 json_parser.py
-go install -v github.com/v2fly/geoip@latest
-geoip
-popd
+export ASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH=go1.21
+go install -v github.com/v2fly/geoip@43aa8a7f3e2ca86f5a3dc4408a5523a5fe8abb1d
+$HOME/go/bin/geoip
